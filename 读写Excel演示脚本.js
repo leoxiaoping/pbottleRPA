7935ba25b378c69b7474a5cfac66766ea6fa6e0e
Leo <leo@pbottle.com>
2022-11-03 12:17:03 +0800

    改进

--- 读写Excel演示脚本.js
+++ 读写Excel演示脚本.js
@@ -13,6 +13,10 @@ pbottleRPA.sleep(1000*5)
 
 
 
+pbottleRPA.tts(`将当前电脑配置信息生成EXCEL文件`)
+pbottleRPA.sleep(1000*5)
+
+
 //写入excel
 const data = [
     ['项', '值'],
@@ -43,8 +47,18 @@ let buffer = xlsx.build([{name: '系统信息表', data: data}],{sheetOptions});
 fs.writeFileSync(`${__dirname}\\Excel测试表格.xlsx`,buffer);
 
 
+pbottleRPA.tts(`已经生成EXCEL文件... `)
+pbottleRPA.sleep(1000*5)
+
+
 //读取excel
 const workSheetsFromFile = xlsx.parse(`${__dirname}\\Excel测试表格.xlsx`);
 const sheet = workSheetsFromFile[0].data;
 console.log(sheet);
+
+pbottleRPA.tts(`读取生成EXCEL文件内容... `)
+pbottleRPA.sleep(1000*5)
+
+
+pbottleRPA.tts('演示结束')
 
