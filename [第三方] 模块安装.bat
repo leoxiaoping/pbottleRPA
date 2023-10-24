b2ba66c04446fa61932dc5c9385e9910a7ee7baf
Leo <leo@pbottle.com>
2023-10-24 16:36:44 +0800

    update

---
+++ [第三方] 模块安装.bat
@@ -0,0 +1,16 @@
+@echo off
+cd .
+
+echo 正在切换国内安装源 
+call npm config set registry https://registry.npmmirror.com/
+echo.
+
+echo 正在安装模块中
+call npm install node-xlsx
+echo.
+
+echo.
+echo 恭喜，安装完成!~    按任意键退出
+
+pause >nul
+
