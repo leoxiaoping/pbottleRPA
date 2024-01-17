d99c8f4388e847eef3a7397e42be92d6d1cfa73a
Leo <leo@pbottle.com>
2024-01-18 00:13:03 +0800

    update

--- 模块新脚本演示（顶级await）.mjs
+++ 模块新脚本演示（顶级await）.mjs
@@ -3,7 +3,7 @@
  * 官网：https://rpa.pbottle.com/
  * 流程开发文档：https://gitee.com/pbottle/pbottle-rpa/wikis/pages
  * 
- * es模块化新脚本，支持顶级 await
+ * es模块化新脚本(.mjs)，支持顶级 await 处理 promise 异步
  */
 import pbottleRPA from "./pbottleRPA.js";  //必须含 .js 后缀
 import path from "path";
@@ -13,7 +13,7 @@ import { fileURLToPath } from 'url'
 if (process.version<'v18') {
     console.log('当前nodejs版本：',process.version);
     pbottleRPA.tts('请升级nodejs v18 以上版本")')
-    pbottleRPA.showMsg('小瓶RPA提示','pbottleRPA.exit("请升级nodejs v18 以上版本")')
+    pbottleRPA.showMsg('小瓶RPA提示','请升级nodejs v18 以上版本')
     pbottleRPA.sleep(3000)
     pbottleRPA.exit("请升级nodejs v18 以上版本")
 }
@@ -24,7 +24,9 @@ global.__dirname = path.dirname(__filename)
 console.log('当前文件名：',__filename)
 console.log('当前路径：',__dirname)
 
+
 try {
+    pbottleRPA.sleep(500)
     console.log('正在请求github.com，请稍后...');
     let res0 = await fetch(`https://github.com/`) //promise 同步
     console.log('github 请求状态:',res0.status);
@@ -33,6 +35,7 @@ try {
     // console.log(error);
 }
 
+pbottleRPA.sleep(500)
 console.log('正在请求本地，请稍后...');
 let res = await fetch(`http://127.0.0.1:49888/`) //promise 同步
 console.log('请求状态:',res.status);
@@ -41,6 +44,6 @@ console.log('请求结果:',html);
 
 
 //语音播报
-pbottleRPA.tts('流程结束！~')
-console.log('流程结束！~');
-pbottleRPA.showMsg('小瓶RPA提示','流程结束！~')
+pbottleRPA.tts('演示流程结束！~')
+console.log('演示流程结束！~');
+pbottleRPA.showMsg('小瓶RPA提示','演示流程结束！~')
