8a1d951ff5c72a2b60bf2b04b39f2101d788436b
Leo <leo@pbottle.com>
2024-08-10 17:36:18 +0800

    [企业版]集群控制中心示例

---
+++ [企业版]集群控制中心示例.js
@@ -0,0 +1,17 @@
+/**
+ * 小瓶RPA演示demo，具体api请查看*流程开发文档*
+ * 官网：https://rpa.pbottle.com/
+ * 流程开发文档：https://gitee.com/pbottle/pbottle-rpa/wikis/pages
+ */
+
+const pbottleRPA = require('./pbottleRPA')
+
+
+//控制中心配置参数从本地 buffer0 中直接读取
+let buffer = pbottleRPA.bufferGet(0) 
+console.log('控制中心任务参数（json格式）：',JSON.parse(buffer));
+
+//流程开始
+let resolution = pbottleRPA.getResolution()
+console.log('当前电脑屏幕分辨率',resolution)
+
