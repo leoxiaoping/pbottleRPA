754260e7e8bb92c1cce52d16176b9e8ac2ffef92
Leo <leo@pbottle.com>
2025-03-07 14:00:23 +0800

    全局变量格式

--- 错误捕获模板（异步主流程）.js
+++ 错误捕获模板（异步主流程）.js
@@ -1,11 +1,14 @@
 const pbottleRPA = require('./pbottleRPA');
 
 
+//全局变量格式
+let global_processName = '小瓶RPA——XXXX流程模板'
+let global_startTime = pbottleRPA.utils.getTime()
 
 //主流程指令序列化
 async function main(params) {
 
-    pbottleRPA.log("主流程开始 📍")  //开始
+    pbottleRPA.log("主流程开始 📍",global_startTime,global_processName)  //开始
 
     await process1() //有错误
     pbottleRPA.log(Date.now())
@@ -19,7 +22,6 @@ main().catch((e)=>{  //主流程错误捕获
 
     console.log('❌ 未完成，错误',e);
     console.log('准备发送消息给管理员 👉');
-
 })
 
 
