6721ec63bafc38b4e507f0b9d4c7cdb46aad6c6e
Leo <leo@pbottle.com>
2025-04-03 15:46:55 +0800

    复制文件+复制文字 测试

--- 剪切板演示脚本.js
+++ 剪切板演示脚本.js
@@ -17,27 +17,24 @@ console.log('超级剪切板','新版剪切板已经支持获取图片、网页�
 //延迟5秒
 pbottleRPA.wait(5)
 
-pbottleRPA.tts('已经将文字复制到您的剪切板，赶紧粘贴试试吧')
-console.log('已经将文字复制到您的剪切板，赶紧粘贴试试吧');
-
+pbottleRPA.tts('已经复制文字，赶紧记事本粘贴试试吧')
+console.log('已经复制文字，赶紧记事本粘贴试试吧');
 pbottleRPA.paste("小瓶RPA官网：https://rpa.pbottle.com/")
 
 
 
 //延迟5秒
-pbottleRPA.wait(5)
+pbottleRPA.wait(10)
 let text = pbottleRPA.getClipboard();
 console.log("获取当前剪切板文本：",text);
 
 
 //
 console.log("复制文件模拟操作：")
-pbottleRPA.copyFile(__dirname + '/pbottleRPA.js')
+pbottleRPA.copyFile(__dirname + '/input/RPAlogo128.png')
 let filepath = pbottleRPA.getClipboard();
 console.log("剪切板文件路径：",filepath);
 
 
-console.log("准备结束脚本");
-//脚本强制退出
-process.exit(1)
-console.log("已经退出了，无效");
+pbottleRPA.tts('已经复制文件，赶紧桌面粘贴试试吧')
+console.log('已经复制文件，赶紧桌面粘贴试试吧');
